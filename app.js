a00aae3ad24f901173c604395371b82922c04b59
Danny Nhoisaykham <danny@Dannys-MacBook-Pro.local>
2018-04-19 23:01:42 -0500

    add pug homepage

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine","pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname,"/views/index.html")));
+app.get("/", (req, res) =>
+  //res.sendFile(path.join(__dirname, "/views/index.html"))
+  res.render("home", {title:"Danny Test"})
+);
 
 app.listen(8080, () => console.log(`port listening on localhost:8080`));
